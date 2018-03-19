91207806a208a1e99f1d1d3bce8c95a82dbf1870
Petar Korudzhiev <petar.korudzhiev@gmail.com>
2018-03-19 22:08:04 +0200

    fix debugging problem

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,28 @@
+const nodeExternals = require('webpack-node-externals');
 const path = require('path');
-// eslint-disable-next-line import/no-unresolved
-//const nodeExternals = require('webpack-node-externals');
 const slsw = require('serverless-webpack');
 
 module.exports = {
-  devtool: "source-map",
+  devtool: 'source-map',
   entry: slsw.lib.entries,
   target: 'node',
-  //externals: [nodeExternals()],
-  module: {
-    /*rules: [
-      {
-        test: /\.js$/,
-        use: [
-          {
-            loader: 'babel-loader'
-          }
-        ],
-      }
-    ]*/   
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel-loader'],
-      include: __dirname,
-      exclude: /node_modules/,
-    }],      
-  },
+  externals: [nodeExternals()],
   output: {
     libraryTarget: 'commonjs',
-    path: path.join(__dirname, '.webpack'),
-    filename: '[name].js',
+    path: path.resolve(__dirname, '.webpack'),
+    filename: '[name].js' // this should match the first part of function handler in serverless.yml
   },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ["babel-loader"]
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  }
 };
